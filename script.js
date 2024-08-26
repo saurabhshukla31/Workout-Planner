42e79457f3bd046c9bb635fe71e7e7b39f84d109
Saurabh Shukla <123800993+saurabhshukla31@users.noreply.github.com>
2024-08-26 12:21:08 +0530

    .

--- script.js
+++ script.js
@@ -1,75 +1,81 @@
 const workouts = {
   Monday: { form: "Rest", exercises: [] },
   Tuesday: {
-    form: "Push",
+    form: "Chest & Back",
     exercises: [
       ["Close Grip Pushups", "Till Failure"],
       ["DB Bench Press", "3 sets", "8-12 reps"],
       ["Barbell Bench Press", "3 sets", "8-12 reps"],
       ["Pec Deck", "3 sets", "10-15 reps"],
-      ["Bar Tricep Pressdown", "3 sets", "10-15 reps"],
-      ["Rope Tricep Pressdown", "3 sets", "10-15 reps"],
-      ["EZ Bar Skull Crusher", "3 sets", "8-12 reps"],
+      ["Assisted Pullups", "Till Failure"],
+      ["Lat Pulldown", "3 sets", "8-12 reps"],
+      ["Seated Cable Row", "3 sets", "8-12 reps"],
+      ["T-bar Row", "3 sets", "8-12 reps"],
     ],
   },
   Wednesday: {
-    form: "Pull",
-    exercises: [
-      ["Assisted Pullups", "Till Failure"],
-      ["Wide Grip Lat Pulldown", "3 sets", "8-12 reps"],
-      ["Bicep Lat Pulldown", "3 sets", "8-12 reps"],
-      ["Short Grip Seated Cable Row", "3 sets", "8-12 reps"],
-      ["DB Curl", "3 sets", "10-15 reps"],
-      ["Preacher Curl", "3 sets", "10-15 reps"],
-      ["Bar Bicep Curl", "3 sets", "10-15 reps"],
-    ],
-  },
-  Thursday: {
-    form: "Legs",
+    form: "Legs & Shoulders",
     exercises: [
       ["Squats", "Till Failure"],
-      ["Machine Shoulder Press", "3 sets", "8-12 reps"],
-      ["Lateral Raises", "3 sets", "10-15 reps"],
-      ["Front Raises", "3 sets", "10-15 reps"],
       ["Smith Machine Squat", "3 sets", "8-12 reps"],
       ["Leg Press", "3 sets", "10-15 reps"],
       ["Leg Extension", "3 sets", "10-15 reps"],
+      ["Reverse Pec Deck", "3 sets", "10-15 reps"],
+      ["Machine Shoulder Press", "3 sets", "8-12 reps"],
+      ["Lateral Raises", "3 sets", "10-15 reps"],
+      ["Front Raises", "3 sets", "10-15 reps"],
+    ],
+  },
+  Thursday: {
+    form: "Arms",
+    exercises: [
+      ["Tricep Pressdown", "3 sets", "10-15 reps"],
+      ["Seated Pronated Grip Bar Curl", "3 sets", "8-12 reps"],
+      ["Seated Dumbbell Curl", "3 sets", "10-15 reps"],
+      ["Bayesian Cable Curl", "3 sets", "10-15 reps"],
+      ["Preacher Curl", "3 sets", "8-12 reps"],
+      ["Hammer Curl", "3 sets", "10-15 reps"],
+      ["Barbell Wrist Curl", "Till Failure"],
+      ["Forearm Wrist Roll", "Till Failure"],
     ],
   },
   Friday: {
-    form: "Push",
+    form: "Chest & Back",
     exercises: [
       ["Close Grip Pushups", "Till Failure"],
       ["DB Bench Press", "3 sets", "8-12 reps"],
       ["Barbell Bench Press", "3 sets", "8-12 reps"],
       ["Pec Deck", "3 sets", "10-15 reps"],
-      ["Bar Tricep Pressdown", "3 sets", "10-15 reps"],
-      ["Rope Tricep Pressdown", "3 sets", "10-15 reps"],
-      ["EZ Bar Skull Crusher", "3 sets", "8-12 reps"],
+      ["Assisted Pullups", "Till Failure"],
+      ["Lat Pulldown", "3 sets", "8-12 reps"],
+      ["Seated Cable Row", "3 sets", "8-12 reps"],
+      ["T-bar Row", "3 sets", "8-12 reps"],
     ],
   },
   Saturday: {
-    form: "Pull",
-    exercises: [
-      ["Assisted Pullups", "Till Failure"],
-      ["Wide Grip Lat Pulldown", "3 sets", "8-12 reps"],
-      ["Bicep Lat Pulldown", "3 sets", "8-12 reps"],
-      ["Short Grip Seated Cable Row", "3 sets", "8-12 reps"],
-      ["DB Curl", "3 sets", "10-15 reps"],
-      ["Preacher Curl", "3 sets", "10-15 reps"],
-      ["Bar Bicep Curl", "3 sets", "10-15 reps"],
-    ],
-  },
-  Sunday: {
-    form: "Legs",
+    form: "Legs & Shoulders",
     exercises: [
       ["Squats", "Till Failure"],
-      ["Machine Shoulder Press", "3 sets", "8-12 reps"],
-      ["Lateral Raises", "3 sets", "10-15 reps"],
-      ["Front Raises", "3 sets", "10-15 reps"],
       ["Smith Machine Squat", "3 sets", "8-12 reps"],
       ["Leg Press", "3 sets", "10-15 reps"],
       ["Leg Extension", "3 sets", "10-15 reps"],
+      ["Reverse Pec Deck", "3 sets", "10-15 reps"],
+      ["Machine Shoulder Press", "3 sets", "8-12 reps"],
+      ["Lateral Raises", "3 sets", "10-15 reps"],
+      ["Front Raises", "3 sets", "10-15 reps"],
+    ],
+  },
+  Sunday: {
+    form: "Arms",
+    exercises: [
+      ["Tricep Pressdown", "3 sets", "10-15 reps"],
+      ["Seated Pronated Grip Bar Curl", "3 sets", "8-12 reps"],
+      ["Seated Dumbbell Curl", "3 sets", "10-15 reps"],
+      ["Bayesian Cable Curl", "3 sets", "10-15 reps"],
+      ["Preacher Curl", "3 sets", "8-12 reps"],
+      ["Hammer Curl", "3 sets", "10-15 reps"],
+      ["Barbell Wrist Curl", "Till Failure"],
+      ["Forearm Wrist Roll", "Till Failure"],
     ],
   },
 };
@@ -102,13 +108,13 @@ function displayCurrentDayWorkout() {
     const exerciseItem = document.createElement("li");
     const exerciseName = document.createElement("span");
     exerciseName.classList.add("exercise-name");
-    exerciseName.textContent = Array.isArray(exercise) ? exercise[0] : exercise;
+    exerciseName.textContent = exercise[0];
     const setCount = document.createElement("span");
     setCount.classList.add("set-count");
-    setCount.textContent = Array.isArray(exercise) ? exercise[1] : "";
+    setCount.textContent = exercise[1] || "";
     const repCount = document.createElement("span");
     repCount.classList.add("rep-count");
-    repCount.textContent = Array.isArray(exercise) ? exercise[2] : "";
+    repCount.textContent = exercise[2] || "";
     exerciseItem.appendChild(exerciseName);
     exerciseItem.appendChild(setCount);
     exerciseItem.appendChild(repCount);
@@ -134,15 +140,13 @@ function displayAllWorkouts() {
       const exerciseItem = document.createElement("li");
       const exerciseName = document.createElement("span");
       exerciseName.classList.add("exercise-name");
-      exerciseName.textContent = Array.isArray(exercise)
-        ? exercise[0]
-        : exercise;
+      exerciseName.textContent = exercise[0];
       const setCount = document.createElement("span");
       setCount.classList.add("set-count");
-      setCount.textContent = Array.isArray(exercise) ? exercise[1] : "";
+      setCount.textContent = exercise[1] || "";
       const repCount = document.createElement("span");
       repCount.classList.add("rep-count");
-      repCount.textContent = Array.isArray(exercise) ? exercise[2] : "";
+      repCount.textContent = exercise[2] || "";
       exerciseItem.appendChild(exerciseName);
       exerciseItem.appendChild(setCount);
       exerciseItem.appendChild(repCount);
